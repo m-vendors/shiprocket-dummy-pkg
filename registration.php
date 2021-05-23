8bba7cd1a1a56e4a33aa22ca50b3358749fb7b8e
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:10:02 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'shiprocket_DummyPkg',
+    __DIR__
+);
